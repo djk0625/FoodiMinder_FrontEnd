5a1b47860ed50e158cf8361ef75b291960125c2b
shson <shson@owensgroup.co.kr>
2024-02-08 14:00:02 +0900

    [20240208] api 호출 예제, 공통 함수 추가

---
+++ src/util/Common.js
@@ -0,0 +1,18 @@
+/**
+ * 공통 함수 정의
+ */
+
+const common = {
+    /**
+    * null check function (if null => return true)
+    * @param {*} value 
+    * @returns {boolean}
+    */
+    checkNull: (value) => {
+        if (value === "" || value === null || value === undefined) {
+            return true;
+        } return false;
+    }
+}
+
+export default common;
